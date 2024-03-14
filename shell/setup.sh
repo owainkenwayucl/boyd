fe747083f399a04e2d206c05121e92f419d9a07b
Owain Kenway <o.kenway@ucl.ac.uk>
2024-03-14 15:46:21 +0000

    Clear out pre environmnet

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,7 +1,7 @@
-BOYD_PYTHON_VER=${BOYD_PYTHON_VER:-"3.8.0"}
+BOYD_PYTHON_VER=${BOYD_PYTHON_VER:-"3.9.18"}
 
-source /etc/profile.d/modules.sh
+# source /etc/profile.d/modules.sh
 
 export BOYD_PYTHON_VER
 
-module load python/${BOYD_PYTHON_VER}
+# module load python/${BOYD_PYTHON_VER}
