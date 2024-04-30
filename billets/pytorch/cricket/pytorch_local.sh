a7bb7671c5070123676cc4ca9473a7dabdd8513b
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-30 14:50:14 +0100

    Remove reliance on things in my ~

--- billets/pytorch/cricket/pytorch_local.sh
+++ billets/pytorch/cricket/pytorch_local.sh
@@ -1,4 +1,9 @@
-source ~/cuda12.3.sh
+export CUDA_HOME=/usr/local/cuda-12.3
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+# FML https://x.com/owainkenway/status/1768295586938122399
+export CPATH=$CUDA_HOME/include:$CPATH
 
 echo ">>> CPATH IS ${CPATH} <<<"
 
