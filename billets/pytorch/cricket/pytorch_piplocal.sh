d992c33a40e52caf64aab32fecc8b346107df7dd
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-30 14:51:20 +0100

    Remove commented out noise in cricket builds

--- billets/pytorch/cricket/pytorch_piplocal.sh
+++
@@ -1,7 +0,0 @@
-#tensort_dir=/home/uccaoke/Applications/TensorRT-8.6.1.6
-
-#pip install ${tensort_dir}/python/tensorrt-8.6.1-cp39-none-linux_aarch64.whl 
-#pip install ${tensort_dir}/python/tensorrt_lean-8.6.1-cp39-none-linux_aarch64.whl 
-#pip install ${tensort_dir}/python/tensorrt_dispatch-8.6.1-cp39-none-linux_aarch64.whl 
-
-#pip install ${tensort_dir}/onnx_graphsurgeon/onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
