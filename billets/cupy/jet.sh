b0f05f8d449ed352a58af176b30d5518e4d78fa8
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-04 17:12:48 +0100

    -m

--- billets/cupy/jet.sh
+++ billets/cupy/jet.sh
@@ -21,7 +21,7 @@ temp_dir=`mktemp -d -p /dev/shm/${me}/${name}`
 
 # Create build environment
 cd $temp_dir
-python3 venv build_venv
+python3 -m venv build_venv
 source build_venv/bin/activate
 pip3 install --upgrade pip
 
