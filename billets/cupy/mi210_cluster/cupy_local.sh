a7435f2b9ae143786db89e8ce65343614968a015
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-05 10:31:51 +0100

    cupy_local for AMD test cluster

---
+++ billets/cupy/mi210_cluster/cupy_local.sh
@@ -0,0 +1,3 @@
+export ROCM_HOME=/opt/rocm
+export HCC_AMDGPU_TARGET=gfx90a
+export CFLAGS="-I/opt/rocm/include/hipblas -I/opt/rocm/include/hipsparse -I/opt/rocm/include/hipfft -I/opt/rocm/include/rocsolver -I/opt/rocm/include/rccl"
