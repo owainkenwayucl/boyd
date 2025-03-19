e272be84009c8742abaab990f35b4f81a18e2f85
Owain Kenway <o.kenway@ucl.ac.uk>
2025-03-19 12:46:06 +0000

    OK, we need to do python3.8 cos stuff is hard coded in

--- billets/tt-buda/jet.sh
+++ billets/tt-buda/jet.sh
@@ -15,7 +15,7 @@ temp_dir=`mktemp -d -p /dev/shm/${me}/${name}`
 
 # Create build environment
 cd $temp_dir
-python3.9 -m venv build_venv
+python3.8 -m venv build_venv
 source build_venv/bin/activate
 pip3 install --upgrade pip wheel setuptools
 # Dependencies for build
